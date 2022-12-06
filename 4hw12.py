235afd38af2482f4659d38f3cfc57988ecfbd390
VictoriiaCher <112928478+VictoriiaCher@users.noreply.github.com>
2022-12-06 13:30:04 +0200

    Add files via upload

---
+++ 4hw12.py
@@ -0,0 +1,27 @@
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        self.filename = filename
+        if contacts is None:
+            self.contacts = []
+        else:
+            self.contacts = contacts
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            list_contacts = pickle.load(file)
+        return list_contacts
